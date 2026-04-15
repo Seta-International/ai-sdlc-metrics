9a4a7b08f4476c8e1d272bcb827972b98846e3f5
Canh Ta <canh.ta@seta-international.vn>
2026-04-15 17:30:56 +0700

    feat(people): add countryCode, workerType, terminationReason to template domain entities

--- apps/api/src/modules/people/domain/entities/offboarding-template.entity.ts
+++ apps/api/src/modules/people/domain/entities/offboarding-template.entity.ts
@@ -6,10 +6,25 @@ export type OffboardingReasonCategory =
   | 'redundancy'
   | 'end_of_contract'
 
+export type TerminationReason =
+  | 'voluntary_resignation'
+  | 'involuntary_performance'
+  | 'involuntary_misconduct'
+  | 'redundancy'
+  | 'end_of_contract'
+  | 'mutual_agreement'
+  | 'retirement'
+  | 'deceased'
+  | 'failed_probation'
+  | 'no_show'
+  | 'company_closure'
+
 export interface OffboardingTemplate {
   id: string
   tenantId: string
   name: string
+  countryCode: string | null
+  terminationReason: TerminationReason | null
   employmentType: EmploymentType | null
   reasonCategory: OffboardingReasonCategory | null
   isDefault: boolean
